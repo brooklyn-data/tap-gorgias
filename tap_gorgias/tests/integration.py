f9f607d6b905b4981f77879796502ec54742cfea
Daniel Palma <daniel.palma@brooklyndata.co>
2022-06-07 11:33:53 +0200

    Clean up tests

--- tap_gorgias/tests/integration.py
+++ tap_gorgias/tests/integration.py
@@ -20,7 +20,6 @@ SAMPLE_CONFIG = {
 # Edit these values to match your environment:
 EXPECTED_RECORD_COUNT = 363
 PAGE_SIZE = 50
-EXPECTED_PAGE_COUNT = EXPECTED_RECORD_COUNT // PAGE_SIZE + 1
 
 
 # Run standard built-in tap tests from the SDK:
@@ -35,30 +34,7 @@ def test_standard_tap_tests():
         test()
 
 
-def test_if_getting_all_records():
-    """Test if we get All Record from the Satisfaction Surveys endpoint."""
-    tap = TapGorgias(config={**SAMPLE_CONFIG, "page_size": PAGE_SIZE}, parse_env_config=True)
-
-    stdout_buf = io.StringIO()
-    stderr_buf = io.StringIO()
-    with redirect_stdout(stdout_buf), redirect_stderr(stderr_buf):
-        streams = tap.load_streams()
-        for stream in streams:
-            if stream.tap_stream_id == "satisfaction_surveys":
-                stream.sync()
-    stdout_buf.seek(0)
-    stderr_buf.seek(0)
-
-    record_count = 0
-    for message in stdout_buf:
-        o = singer.parse_message(message).asdict()
-        if o['type'] == 'RECORD':
-            record_count += 1
-
-    assert record_count == EXPECTED_RECORD_COUNT
-
-
-def test_paging():
+def get_all_records():
     tap = TapGorgias(config={**SAMPLE_CONFIG, "page_size": PAGE_SIZE}, parse_env_config=True)
 
     page_count = 0
@@ -76,11 +52,27 @@ def test_paging():
     with redirect_stdout(stdout_buf), redirect_stderr(stderr_buf):
         streams = tap.load_streams()
         for stream in streams:
-            # Restrict to the Satisfaction Surveys stream as this usually is small to query and has multiple pages.
             if stream.tap_stream_id == "satisfaction_surveys":
                 stream.prepare_request = counter(stream.prepare_request)
                 stream.sync()
     stdout_buf.seek(0)
     stderr_buf.seek(0)
 
-    assert page_count == EXPECTED_PAGE_COUNT
+    record_count = 0
+    for message in stdout_buf:
+        o = singer.parse_message(message).asdict()
+        if o['type'] == 'RECORD':
+            record_count += 1
+    return record_count, page_count
+
+
+def test_if_getting_all_records():
+    """Test if we get All Record from the Satisfaction Surveys endpoint."""
+    all_record_count, page_count = get_all_records()
+    assert all_record_count == EXPECTED_RECORD_COUNT
+
+
+def test_paging():
+    """Test if we get every page from the Satisfaction Surveys endpoint."""
+    all_record_count, page_count = get_all_records()
+    assert all_record_count // PAGE_SIZE + 1 == page_count
